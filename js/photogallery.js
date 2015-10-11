21555a808c52c6f5f871ea8976a09966f12587d7
koreoliver <kore.oliver@gmail.com>
2015-10-11 14:50:15 -0700

    fix archive js on clicking main image

--- js/photogallery.js
+++ js/photogallery.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){        
-	$('li img').on('click',function(){
+	$('#archive li img').on('click',function(){
 		var src = $(this).attr('src');
 		var img = '<img src="' + src + '" class="img-responsive"/>';
 		
