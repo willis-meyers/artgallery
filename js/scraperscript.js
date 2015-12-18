de9a153b630bdd05d5eac2a485bdba8ed3e8a904
SofyaMulenok <mulenoksv@gmail.com>
2015-12-18 14:02:38 -0800

    fixed scraper link

--- js/scraperscript.js
+++ js/scraperscript.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$.get("https://api.import.io/store/data/20073f65-5520-431f-8ba9-a798e4d4c202/_query?input/webpage/url=http%3A%2F%2Foaklandartmurmur.org%2Fexhibitions%2F&_user=e12292e8-0b26-4761-8748-3516c5636791&_apikey=e12292e80b26476187483516c5636791e741d7925906da53c11bda587e6bf4dfd4cde8255e62266a19e44e717ab91bdb1ada7c358185ebaa049ab060e19dca9ba51b8d20de4f82444440e59d083609e4", 
+	$.get("https://api.import.io/store/connector/b9960a2d-ab98-4c7e-88ce-868c04aa0c5a/_query?input=webpage/url:http%3A%2F%2Foaklandartmurmur.org%2Fexhibitions%2F&&_apikey=0e440761c7de4ef78db3614c91e340856af82956e5fb5a325401714758d96a472c2b157eaa4684f0a924f82a044f90249f3620cbcfb877616b623865237b1a92571dca5e524b9448c0f3ba63ea392e23", 
 		function( data ) {
 		var returnedEvents = data;
 		results = returnedEvents.results;
